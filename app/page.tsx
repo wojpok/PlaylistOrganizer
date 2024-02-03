9482e6ee71c5486ccca99a563fd7ed5d5827064e
wojpok <324526@uwr.edu.pl>
2024-02-03 15:39:27 +0100

    ignore build for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import App from "@/components/App/App";
+import App from "../components/App/App";
 
 export default function Home() {
   return (
